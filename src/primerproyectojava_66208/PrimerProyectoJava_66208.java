e16a328853c0baef610a4d64e465568241c2a980
RicardoMoo <RicardoMoo@LAPTOP-QLOP7A9S>
2021-03-03 08:35:32 -0600

    primer proyecto de java

---
+++ src/primerproyectojava_66208/PrimerProyectoJava_66208.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyectojava_66208;
+
+/**
+ *
+ * @author RicardoMoo
+ */
+public class PrimerProyectoJava_66208 {
+
+    public static void main(String[] args) {
+       int variableEntero = 66;
+       char variableCaracter = ('B');
+       boolean variableBolean = false;
+       
+    }
+    
+}
